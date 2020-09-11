decc07d77e735226c6aa23dac2b96986ef3bf5c5
riyaz <riyazpasha880@gmail.com>
2020-09-11 11:31:52 +0530

    intial set up

---
+++ src/main/java/com/example/tddspringboot/TddSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tddspringboot/TddSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
